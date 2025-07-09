a9491be8278a05db79190801ce9179d7df236cae
Pejmanaghapour <aghapour.pejman@gmail.com>
2025-07-09 22:48:44 +0330

    Update layout.tsx

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -1,3 +1,7 @@
-export default function ChatLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+export default function ChatLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>{children}</div>;
 }
